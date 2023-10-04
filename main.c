617d2a61108e5f75f064a498e455161b7a89d7ec
sungha <choish0717@sookmyung.ac.kr>
2023-10-04 16:39:26 +0900

    4-2

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char* argv[]) {
+	int a, b;
+		printf("input two intergers : ");
+		scanf("%i %i", &a, &b);
 
-int main(int argc, char *argv[]) {
-	int x,y,z,m;
-	int a,b,c;
-	x = 2;
-	z = 1;
-	a = 3;
-	b = 4;
-	c = 5;
-	
-	y = a*x*x + b*x + c;
-	m = (x + y + z)/3;
-	printf("y = %d, m = %d",y,m);
+
+	printf("%i + %i = %i\n", a ,b ,a + b);
+	printf("%i - %i = %i\n", a, b, a - b);
+	printf("%i * %i = %i\n", a, b, a * b);
+	printf("%i / %i = %i\n", a, b, a / b);
+	printf("%i %% %i = %i\n", a, b, a % b);
 	return 0;
 }
