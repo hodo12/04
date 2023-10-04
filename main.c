9247f0340a045414630e96fccd2d9af46f70d018
sungha <choish0717@sookmyung.ac.kr>
2023-10-04 16:57:31 +0900

    4-4

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-	int s;
-	
-      printf("input the second : ");
-      scanf("%i", &s);
+int y;
+		printf("input the year : ");
+		scanf("%i", &y);
 
-      printf("the time is %i : %i", s / 60, s %60);
+		if ((y % 4 == 0  &&  y % 100 != 0) || (y % 400 == 0)) {
+			printf("%i is leap year.\n", y);
+		}
+		else
+		{
+			printf("%i is not leap year.\n", y);
+		}
 
 	return 0;
 }
